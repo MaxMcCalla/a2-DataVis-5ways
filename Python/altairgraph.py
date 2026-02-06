acc0e04157d65671a1a9cf1877362130cfd55501
MaxMcCalla <36572894+MaxMcCalla@users.noreply.github.com>
2026-02-05 23:41:21 -0500

    Finished d3, added altair

--- Python/altairgraph.py
+++ Python/altairgraph.py
@@ -9,12 +9,17 @@ df = pd.read_csv(url)
 
 print(df)
 
+domain = ['Adelie', "Gentoo", "Chinstrap"]
+range_ = ['#f09600', '#00a08d', '#8c00b4']
+
 alt.renderers.enable('mimetype')
 
-chart = alt.Chart(df).mark_point().encode(
-    x='flipper_length_mm',
-    y='body_mass_g',
-    size='bill_length_mm'
+chart = alt.Chart(df).mark_point(filled=True,fillOpacity=0.6,strokeOpacity=1,strokeWidth=1).encode(
+    alt.X('flipper_length_mm').scale(zero=False),
+    alt.Y('body_mass_g').scale(zero=False),
+    alt.Size('bill_length_mm').scale(domain=(30, 50)),
+    color=alt.Color('species').scale(domain=domain,range=range_),
+    stroke=alt.Color('species').scale(domain=domain,range=range_)
 )
 
 chart.save('chart.html')
